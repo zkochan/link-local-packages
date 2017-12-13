c09088edb65d3db6d054b495e3dd24ba62e1ecbe
Zoltan Kochan <z@kochan.io>
2017-12-13 23:15:25 +0200

    test: use package-preview

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,6 @@
 import test = require('tape')
 import path = require('path')
-import linkLocalPackages from '../src'
+import linkLocalPackages from 'link-local-packages'
 
 test('links local packages', t => {
   const root = path.join(__dirname, 'fixture')
