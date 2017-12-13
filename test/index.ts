7ca57446c080e53b11918bdc4a8e600d86c91a91
Zoltan Kochan <z@kochan.io>
2017-12-13 23:14:18 +0200

    chore: update dependencies

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import test = require('tape')
 import path = require('path')
 import linkLocalPackages from '../src'
 
-test('links local packages', async t => {
+test('links local packages', t => {
   const root = path.join(__dirname, 'fixture')
   linkLocalPackages(root)
     .then(() => t.end())
